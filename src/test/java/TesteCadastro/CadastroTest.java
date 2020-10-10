0add7177e5226ea80b1b6e302793042e1a0123df
Ricardo <r.c.d.1985@hotmail.com>
2020-10-09 23:58:43 -0300

    foi criado test runner

--- src/test/java/TesteCadastro/CadastroTest.java
+++ src/test/java/TesteCadastro/CadastroTest.java
@@ -6,8 +6,13 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import ricardomello.desafioqaMobile.utils.ReportExtentReports;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", tags = "@Cadastro", 
-glue = "ricardomello.desafioqa.steps", monochrome = true, dryRun = false)
+@CucumberOptions(features = "src/test/resources", tags = "@Cadastro",
+		plugin = {"pretty",
+				"html:target/cucumber",
+				"json:target/cucumber-report/cucumber.json",
+				"json:target/cucumber.json",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter: test-output/report.html"},
+        glue = "ricardomello.desafioqa.steps", monochrome = true, dryRun = false)
 public class CadastroTest {
 	@BeforeClass
 	public static void iniciarReport() {
